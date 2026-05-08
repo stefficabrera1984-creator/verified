3cac67ffa33f87cd6c45e4ad9031ea4df24779af
Rachna Singh <rachnav.singh.1984@gmail.com>
2026-05-08 23:45:34 +0530

    Enable App Router

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // IMPORTANT for static hosting
-
-  images: {
-    unoptimized: true, // REQUIRED for static export
-    domains: [
-      "127.0.0.1",
-      "verifiedequalaccess.com", // your live domain
-    ],
+  experimental: {
+    appDir: true,
   },
 };
 
